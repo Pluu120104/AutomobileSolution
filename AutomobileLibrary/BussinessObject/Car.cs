05ad239ce353dc25e413a20ba76ff7931339c86f
Pluu120104 <phongluu120104@gmail.com>
2024-06-02 15:59:48 +0700

    Update Car.cs

--- AutomobileLibrary/BussinessObject/Car.cs
+++ AutomobileLibrary/BussinessObject/Car.cs
@@ -13,5 +13,6 @@ namespace AutomobileLibrary.BussinessObject
         public string Manufacturer { get; set; }
         public decimal Price { get; set; }
         public int ReleaseYear { get; set; }
+        public DateTime ReleaseDate { get; set; }
     }
 }
